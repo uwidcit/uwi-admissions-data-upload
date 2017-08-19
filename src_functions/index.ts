c27609c7f1fe0d058bda4377a93d8e4018eec73c
Jonathan Herbert <foohyfooh@gmail.com>
2017-08-19 11:49:54 -0400

    Add Post Processing Step for Programmes
    Changes
    - Use Firebase Cloud Function to create an index to search for subjects based on required subjects.

--- src_functions/index.ts
+++ src_functions/index.ts
@@ -19,8 +19,7 @@ import { RowValidator } from './validators/validators'
  */
 function cleanProgrammeValues(programme: Programme) : Programme{
   for(let x in programme) {
-    if(typeof(programme[x]) === 'string') continue;
-    else if(isNaN(programme[x])) delete programme[x];
+    if(typeof(programme[x]) !== 'string' && isNaN(programme[x])) delete programme[x];
   }
   return programme;
 }
@@ -32,27 +31,27 @@ function cleanProgrammeValues(programme: Programme) : Programme{
  */
 function createProgrammeFromRow(row: Row): Programme {
   let programme = 
-     {Degree1:                    row[ 0],
-      Degree2:                    row[ 1],
-      Programme:                  row[ 2],
-      Faculty:                    row[ 3],
+     {Degree1:                    row[ 0].trim(),
+      Degree2:                    row[ 1].trim(),
+      Programme:                  row[ 2].trim(),
+      Faculty:                    row[ 3].trim(),
       FullTime: !!Number.parseInt(row[ 4]),
       PartTime: !!Number.parseInt(row[ 5]),
       Evening:  !!Number.parseInt(row[ 6]),
       CSECPasses: Number.parseInt(row[ 7]),
-      CSECMandatory:              row[ 8],
-      CSECAny1of:                 row[ 9],
-      CSECAny2of:                 row[10],
-      CSECAny3of:                 row[11],
-      CSECAny4of:                 row[12],
-      CSECAny5of:                 row[13],
+      CSECMandatory:              row[ 8].trim(),
+      CSECAny1of:                 row[ 9].trim(),
+      CSECAny2of:                 row[10].trim(),
+      CSECAny3of:                 row[11].trim(),
+      CSECAny4of:                 row[12].trim(),
+      CSECAny5of:                 row[13].trim(),
       CAPEPasses: Number.parseInt(row[14]),
-      CAPEMandatory:              row[15],
-      CAPEAny1of:                 row[16],
-      CAPEAny2of :                row[17],
-      CAPEAny3of:                 row[18],
-      CAPEAny4of:                 row[19],
-      CAPEAny5of:                 row[20],
+      CAPEMandatory:              row[15].trim(),
+      CAPEAny1of:                 row[16].trim(),
+      CAPEAny2of :                row[17].trim(),
+      CAPEAny3of:                 row[18].trim(),
+      CAPEAny4of:                 row[19].trim(),
+      CAPEAny5of:                 row[20].trim(),
       AlternativeQualifications:  row[21],
       OtherRequirements:          row[22],
       Description:                row[23]};
@@ -64,30 +63,25 @@ function createProgrammeFromRow(row: Row): Programme {
  * @param csv 
  */
 function parseCSV(csv: string) : Promise<void>{
-  return Promise.resolve(babyParse.parse(csv))
-  .then(parsed => parsed.data)
-  .then(rows => {
-    // console.log('Headings', rows[0]);
-    let programmes: Programme[] = [];
-    let rowErrors: RowError[] = [];
-    for(let i = 1, row = rows[i], end = rows.length - 1; i < end; i++, row = rows[i]){
-      let rowError = RowValidator.validateRow(i + 1, row);
-      if(rowError === null){
-        let programme = createProgrammeFromRow(row);
-        programmes.push(cleanProgrammeValues(programme));
-      }else{
-        rowErrors.push(rowError);
-      }
+  let result = babyParse.parse(csv);
+  let rows = result.data;
+  // console.log('Headings', rows[0]);
+  let programmes: Programme[] = [];
+  let rowErrors: RowError[] = [];
+  for(let i = 1, row = rows[i], end = rows.length - 1; i < end; i++, row = rows[i]){
+    let rowError = RowValidator.validateRow(i + 1, row);
+    if(rowError === null){
+      let programme = createProgrammeFromRow(row);
+      programmes.push(cleanProgrammeValues(programme));
+    }else{
+      rowErrors.push(rowError);
     }
-    // console.log(programmes, rowErrors);
-    let rootRef = database.ref('/'), programmesRef = rootRef.child('/Programmes'), rowErrorsRef = rootRef.child('/RowErrors');
-    // programmesRef.remove();
-    // rowErrorsRef.remove()
-    return rootRef.update({
-      Programmes: programmes,
-      RowErrors: rowErrors,
-      updateTime: Date.now()
-    });
+  }
+  // console.log(programmes, rowErrors);
+  return database.ref('/').update({
+    Programmes: programmes,
+    RowErrors: rowErrors,
+    updateTime: Date.now()
   });
 }
 
@@ -123,12 +117,50 @@ expressApp.post('/upload_csv', multer.single('csv'), (request, response, next) =
 
 exports.uploadCSV_https = functions.https.onRequest(expressApp);
 
+exports.uploadCSVPostProcess = functions.database.ref('/Programmes').onWrite(event => {
+  if(!event.data.exists()) return;
+
+  let programmes: Programme[] = event.data.val();
+  let updates = {};
+
+  const addToUpdates = (subject: string, key: string, programme: Programme) => {
+    if(updates[subject] === undefined) updates[subject] = {};
+    updates[subject][key] = programme;  
+  };
+  
+  for(let programme of programmes){
+    let programmeKey = programme.Faculty + programme.Degree2 + programme.Programme;
+
+    programme.CSECMandatory.split(',')
+    .concat(programme.CSECAny1of.split(','))
+    .concat(programme.CSECAny2of.split(','))
+    .concat(programme.CSECAny3of.split(','))
+    .concat(programme.CSECAny4of.split(','))
+    .concat(programme.CSECAny5of.split(','))
+    .map(subject => subject.trim().replace('.', ''))
+    .filter(subject => subject)
+    .map(subject => `CSEC_${subject}`)
+    .forEach(subject => addToUpdates(subject, programmeKey, programme));
+
+    programme.CAPEMandatory.split(',')
+    .concat(programme.CAPEAny1of.split(','))
+    .concat(programme.CAPEAny2of.split(','))
+    .concat(programme.CAPEAny3of.split(','))
+    .concat(programme.CAPEAny4of.split(','))
+    .concat(programme.CAPEAny5of.split(','))
+    .map(subject => subject.trim().replace('.', ''))
+    .filter(subject => subject)
+    .map(subject => `CAPE_${subject}`)
+    .forEach(subject => addToUpdates(subject, programmeKey, programme));
+  }
+
+  return database.ref('/search').set(updates);
+});
+
 exports.newUser = functions.auth.user().onCreate(event => {
     let user = event.data;
     let { uid, email } = user;
-
-    let userRef = database.ref(`/users/${uid}`);
-    return userRef.update({
+    return database.ref(`/users/${uid}`).update({
       uid: uid,
       emial: email,
       name: `User ${uid}`
